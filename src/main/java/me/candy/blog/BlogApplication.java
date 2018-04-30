ce3f7f1b07622e253d964d61e2959fd38770d59a
TWICEMOMO <rlaehdwn3331@gmail.com>
2018-04-30 22:30:44 +0900

    first

---
+++ src/main/java/me/candy/blog/BlogApplication.java
@@ -0,0 +1,21 @@
+package me.candy.blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class BlogApplication {
+
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogApplication.class, args);
+	}
+
+	@GetMapping
+	public String hello(){
+		return "hello world";
+	}
+}
